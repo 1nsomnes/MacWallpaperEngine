cc8ac1035d8e672d8c05596ed1dd79ca3678d376
1nsomnes <1nsomnes@users.noreply.github.com>
2025-06-09 17:44:55 -0700

    Initial Commit

---
+++ MacWallpaperEngine/MacWallpaperEngineApp.swift
@@ -0,0 +1,17 @@
+//
+//  MacWallpaperEngineApp.swift
+//  MacWallpaperEngine
+//
+//  Created by Cedric Claessens on 6/9/25.
+//
+
+import SwiftUI
+
+@main
+struct MacWallpaperEngineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MacWallpaperEngineTests/MacWallpaperEngineTests.swift
@@ -0,0 +1,17 @@
+//
+//  MacWallpaperEngineTests.swift
+//  MacWallpaperEngineTests
+//
+//  Created by Cedric Claessens on 6/9/25.
+//
+
+import Testing
+@testable import MacWallpaperEngine
+
+struct MacWallpaperEngineTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
